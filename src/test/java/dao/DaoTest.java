56318604b6ba4ed1fe117e5f0fd695b26b4ca85e
temporario <temporario@L4C22W.lab.cct.uenp.edu.br>
2022-10-26 11:09:21 -0300

    .

---
+++ src/test/java/dao/DaoTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package dao;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import modelo.Usuario;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author temporario
+ */
+public class DaoTest {
+    
+    public DaoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /*@Test
+    public void testInserirUsuario() {
+        Dao<Usuario> dao = new Dao(Usuario.class);
+        Usuario u = new Usuario("Astolfo", "Rua das Antas, 1 - Bandeirantes-PR", "(43) 9988-7755", 
+                new GregorianCalendar(2004, Calendar.FEBRUARY, 11).getTime(), 
+                "astolfo", "123"); 
+        dao.inserir(u);     
+    }*/
+    
+    
+    
+}
